385d4b0482f74cbfc81aa19b2761edaacc03b055
alilynnh <90108950+alilynnh@users.noreply.github.com>
2021-12-30 13:10:15 -0600

    5.4 done

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,7 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
-// Building data table
+// BUILD DATA TABLE
 function buildTable(data) {
     // clear table
     tbody.html("");
@@ -13,7 +13,7 @@ function buildTable(data) {
     data.forEach((dataRow) => {  
         // append a row to table           
         let row = tbody.append("tr");  
-        
+
         // add value from each object into a cell     
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
@@ -22,3 +22,24 @@ function buildTable(data) {
 
     });
 }
+
+// FILTER BUTTON
+function handleClick() {
+    // get datetime value
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // check if date was entered and filter data using date
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // rebuild table using filtered data
+    buildTable(filteredData);
+};
+
+// listen for button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build table when page loads
+buildTable(tableData);
